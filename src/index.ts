9e3975651187b8545b759c20dafbe5c8bdde6fd0
HarenaFiantso <fiantsorav@gmail.com>
2024-03-02 11:45:57 +0300

    chore: configure mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Express, Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
+import * as mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,10 +7,17 @@ dotenv.config();
 const app: Express = express();
 const port: string | number = process.env.PORT || 3000;
 
+const DB: string = 'mongodb://localhost:27017/chat_app';
+
+mongoose
+    .connect(DB, {})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('MongoDB connection error:', error));
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+    res.send('Hello, Express.js with MongoDB!');
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
